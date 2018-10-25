7624f88772e0a623eca8b6a99c23d013ce1388a4
antoine.torre <torreantoine1@gmail.com>
2018-10-25 19:09:28 +0200

    [DHCP] Add last option (255) and improve mac adress print

--- Dhcp.java
+++ Dhcp.java
@@ -7,7 +7,7 @@ public class Dhcp{
 	private Map<String, String> options;
 
 Dhcp(byte[] datagram){
-	this.details = new HashMap<String, String>();
+	this.details = new LinkedHashMap<String, String>();
 	this.details.put("op", (datagram[0] & 0xFF) + "");
 	this.details.put("htype", String.format("%02X", datagram[1]));
 	this.details.put("hlen", (datagram[2] & 0xFF) + "");
@@ -19,7 +19,7 @@ Dhcp(byte[] datagram){
 	this.details.put("FutureIpClient", Utils.byteToIP(Arrays.copyOfRange(datagram, 16, 20)));
 	this.details.put("IpNextServ", Utils.byteToIP(Arrays.copyOfRange(datagram, 20, 24)));
 	this.details.put("IpRelay", Utils.byteToIP(Arrays.copyOfRange(datagram, 24, 28)));
-	this.details.put("MacClient", Utils.byteToMac(Arrays.copyOfRange(datagram, 28, 44)));
+	this.details.put("MacClient", Utils.byteToMac(Arrays.copyOfRange(datagram, 28, 34)));
 	this.details.put("ServerName", Utils.byteToHex(Arrays.copyOfRange(datagram, 44, 108)));
 	this.details.put("File", Utils.byteToHex(Arrays.copyOfRange(datagram, 108, 236)));
 	this.details.put("MagicCookie", Utils.byteToHex(Arrays.copyOfRange(datagram, 236, 240)));
@@ -27,16 +27,17 @@ Dhcp(byte[] datagram){
 }
 
 private void parseOptions(byte[] datagram){
-	options = new HashMap<String, String>();
+	options = new LinkedHashMap<String, String>();
 	int cursor = 0;
 	int length;
 	int code = (int)Utils.byteToIntBE(Arrays.copyOfRange(datagram, cursor, cursor + 1));
-	while (cursor < datagram.length && code != 255){
+	while (code != 255){
 		length = (int)Utils.byteToIntBE(Arrays.copyOfRange(datagram, cursor + 1, cursor + 2));
 		this.options.put(code + "", Utils.byteToHex(Arrays.copyOfRange(datagram, cursor + 2, cursor + 2 + length)));
 		cursor += 2 + length;
 		code = (int)Utils.byteToIntBE(Arrays.copyOfRange(datagram, cursor, cursor + 1));
 	}
+	this.options.put(code + "", "0");
 }
 
 public String print(){
